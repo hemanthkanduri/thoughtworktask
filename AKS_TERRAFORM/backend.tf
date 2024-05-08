4807825a9c83c74ea6ad1f024c2c308f401f7596
hemanthkanduri <hemanthkanduri@gmail.com>
2024-05-08 15:39:18 +0530

    backend config

--- AKS_TERRAFORM/backend.tf
+++ AKS_TERRAFORM/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "devopsrg"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "aksterraformstg01"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "terraformstatefilecont"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
--- AKS_TERRAFORM/providers.tf
+++ AKS_TERRAFORM/providers.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  client_id       = "2e28e784-7d0f-4a8c-8809-994f81dae684"
+  client_secret   = "TAO8Q~OveqSsy~r2gQH_npPTEwDeVRtjnM2JSdeR"
+  tenant_id       = "db3e7f43-a51d-4d0d-b161-445ca525c334"
+  subscription_id = "efbc1e1f-72e7-41b7-b61b-af618f7575c4"
 }
 
 terraform {
